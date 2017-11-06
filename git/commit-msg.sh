1ffaac6c3c9f2a7c31733656b4836ba7600ddee9
Yusuf Fadairo <yusuf.kami@gmail.com>
2017-11-06 15:08:52 -0500

    Add description for commit msg regex

--- git/commit-msg.sh
+++ git/commit-msg.sh
@@ -2,6 +2,7 @@
 # Should be linked with .git/hooks/commit-msg
 printf "\nValidating commit message format...\n\n"
 
+# Regex for the commit message
 commit_message_regex='^\s-[[:digit:]]{3,4}\s-\s\w+(\s&\s\w+)?:\s.+$'
 
 if ! grep -Eq "$commit_message_regex" "$1"; then
