61aa3ec3e9f43cebc1f60b409ecb34db00135518
Yusuf Fadairo <yusuf.fadairo@telus.com>
2017-11-06 15:05:41 -0500

    Add AWS dofile folder

---
+++ aws/profile.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+# AWS Profile Manager
+# Given an AWS profile name (yusuf-aws, starbeast-aws  , etc.) it uses the aws config parameters
+# for the appropriate account
+
+# Input Parameters
+# 1. Profile name
