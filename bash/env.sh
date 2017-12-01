96befc4e13e7e526b7a88e53abfc4ca1046610fe
Yusuf Fadairo <yusuf.kami@gmail.com>
2017-12-01 13:40:00 +0000

    remove NVM variables from env

--- bash/env.sh
+++ bash/env.sh
@@ -5,11 +5,6 @@ printf '\nMarking commander script\n'
 export ENV_COMMAND=$0
 export ENV_SET_BY_SCRIPT=true
 
-printf '\nUsing NVM_DIR="$HOME/.nvm"\n'
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
 printf '\nUsing default Vault address\n'
 echo 'Using: VAULT_ADDR=http://127.0.0.1:8200'
 export VAULT_ADDR=http://127.0.0.1:8200
