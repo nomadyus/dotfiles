330e21313e89fef72667ebdca31f39dff8d7092e
Yusuf Fadairo <yusuf.kami@gmail.com>
2018-03-20 22:54:12 -0400

    clean up pre-push hook

--- git/pre-push.sh
+++ git/pre-push.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 # Should be linked with .git/hooks/pre-push
+
+# Exit immediately if a command exits with a non-zero status.
 set -e
+
 PATH=$PATH:/usr/local/bin:/usr/local/sbin
 TEST_PATH=./src
 FUNCTIONAL_TEST_PATH=./functional-test
+
 printf "\nRunning pre-push hook...\n\n"
 
-check_for_onlys() {
+checkForOnlys() {
     if grep --quiet --recursive --exclude-dir=node_modules '\.only(' "$1"; then
       echo 'Oh poop! .only found on tests, please remove before pushing:\n'
       grep -H --line-number --recursive --exclude-dir=node_modules '\.only' "$1" | awk '{print "💩 " $0}'
@@ -15,8 +19,8 @@ check_for_onlys() {
     fi
 }
 
-check_for_onlys $TEST_PATH
-check_for_onlys $FUNCTIONAL_TEST_PATH
+checkForOnlys $TEST_PATH
+checkForOnlys $FUNCTIONAL_TEST_PATH
 
 npm install
 npm test
