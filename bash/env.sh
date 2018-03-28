c577078e29503b17dee01c38c1974dabdfe2cede
Yusuf Fadairo <yusuf.kami@gmail.com>
2018-03-28 13:57:25 -0400

    include terminal prompt_command for setting up terminal display properties

--- bash/env.sh
+++ bash/env.sh
@@ -11,3 +11,31 @@ export VAULT_ADDR=http://127.0.0.1:8200
 
 printf '\nSetting flag not to use New Relic config file\n'
 export NEW_RELIC_NO_CONFIG_FILE=true
+
+## Export properties for the terminal settings
+# Locale
+export LC_ALL=en_US.UTF-8
+export LANG="en_US"
+
+# Colors
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+YELLOW="$(tput setaf 3)"
+BLUE="$(tput setaf 4)"
+MAGENTA="$(tput setaf 5)"
+CYAN="$(tput setaf 6)"
+WHITE="$(tput setaf 7)"
+GRAY="$(tput setaf 8)"
+BOLD="$(tput bold)"
+UNDERLINE="$(tput sgr 0 1)"
+INVERT="$(tput sgr 1 0)"
+NOCOLOR="$(tput sgr0)"
+
+# Tell ls to be colourful
+export CLICOLOR=1
+
+# Set colors to match iTerm2 Terminal Colors
+export TERM=xterm-256color
+
+# File system color scheme
+export LSCOLORS=Gxfxcxdxbxegedabagacad
--- dotfiles.sh
+++ dotfiles.sh
@@ -7,6 +7,7 @@ BASH_DIR="${DIR}/bash"
 ALIASES="aliases.sh"
 ENVIRONMENT="env.sh"
 DOTFILES="dotfiles.sh"
+PROFILE="profile.sh"
 
 ## Export this dotfiles to evironment variables
 export DOTFILES="${DIR}/${DOTFILES}"
@@ -18,7 +19,7 @@ if [ -f "${BASH_DIR}/${ALIASES}" ]; then
   ln -fs "${BASH_DIR}/${ALIASES}" "${HOME_DIR}/.${ALIASES}"
   source "${HOME_DIR}/.${ALIASES}"
   printf "Sourced aliases successfully.\n"
-else 
+else
   printf "The Aliases script (${BASH_DIR}/${ALIASES}) doesn't exist.\n"
 fi
 
@@ -27,8 +28,17 @@ if [ -f "${BASH_DIR}/${ENVIRONMENT}" ]; then
   ln -fs "${BASH_DIR}/${ENVIRONMENT}" "${HOME_DIR}/.${ENVIRONMENT}"
   source "${HOME_DIR}/.${ENVIRONMENT}"
   printf "Sourced environment variables successfully.\n"
-else 
+else
   printf "The Environment variable script (${BASH_DIR}/${ENVIRONMENT}) doesn't exist.\n"
+fi
+
+if [ -f "${BASH_DIR}/${PROFILE}" ]; then
+  printf "\nRunning the Profile variable script (${BASH_DIR}/${PROFILE}).\n"
+  ln -fs "${BASH_DIR}/${PROFILE}" "${HOME_DIR}/.${PROFILE}"
+  source "${HOME_DIR}/.${PROFILE}"
+  printf "Sourced profile script successfully.\n"
+else
+  printf "The Profile script (${BASH_DIR}/${PROFILE}) doesn't exist.\n"
 fi
 
 printf "\nThe dotfiles setup is now complete.\n"
