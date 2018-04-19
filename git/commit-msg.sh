6b1b9b3c509378ae77dd94fd76747b845c40b260
Yusuf Fadairo <yusuf.kami@gmail.com>
2018-04-19 11:11:11 -0400

    include note for linking git hooks

--- git/commit-msg.sh
+++ git/commit-msg.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 # Should be linked with .git/hooks/commit-msg
+# You can pferform the symlink with the command:
+#   rm .git/hooks/*
+#   ln -fsv "$(PWD)/git/pre-push" .git/hooks/pre-push
+#   ln -fsv "$(PWD)/git/commit-msg" .git/hooks/commit-msg
 printf "\nValidating commit message format...\n\n"
 
 # Regex for the commit message
