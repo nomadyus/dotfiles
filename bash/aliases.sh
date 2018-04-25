464bbc643624bea89c732dda036390158532218f
Yusuf Fadairo <yusuf.kami@gmail.com>
2018-04-25 13:41:15 -0400

    update getport alias

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -115,4 +115,4 @@ alias dkl="echo 'Running: docker kill'; docker kill "
 ## Magic
 alias psa="echo 'Running: ps aux'; ps aux"
 alias psg="echo 'Running: ps aux | grep '; ps aux | grep "
-alias pan="echo 'Getting port'; getPort"
+alias pan="echo 'Getting port for process'; getPort"
