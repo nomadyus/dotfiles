4fedb62a4bb5d10681aac5cc7f583ce32bfccdd4
Yusuf Fadairo <yusuf.kami@gmail.com>
2018-07-01 12:34:42 -0400

    update profile

--- aws/profile.sh
+++ aws/profile.sh
@@ -2,7 +2,7 @@
 set -e
 
 # AWS Profile Manager
-# Given an AWS profile name (yusuf-aws, starbeast-aws  , etc.) it uses the aws config parameters
+# Given an AWS profile name (yusuf, starbeast, etc.) it uses the aws config parameters
 # for the appropriate account
 
 # Input Parameters
