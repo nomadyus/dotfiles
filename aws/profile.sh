9d9a5347833af7af523259874c6afb428097d462
Yusuf Fadairo <yusuf.kami@gmail.com>
2018-07-21 18:35:03 -0400

    remove aws profile

--- aws/profile.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-
-# AWS Profile Manager
-# Given an AWS profile name (yusuf, starbeast, etc.) it uses the aws config parameters
-# for the appropriate account
-
-# Input Parameters
-# 1. Profile name
